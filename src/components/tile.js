1286778d73cdfd99d6a3fa6586840ddb3cf2f92e
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2018-04-21 07:26:12 +0200

    Refactor tile component

--- src/components/tile.js
+++ src/components/tile.js
@@ -1,6 +1,4 @@
-const CYLINDER_RADIUS = 5
-const ANGLE_STEP = 12
-const CAMERA_OFFSET = 2.5
+import { toRadians } from '../utils'
 
 export const tile = {
   schema: {
@@ -10,28 +8,8 @@ export const tile = {
     ncol: { type: 'number' }
   },
   init () {
-    const { row, col, nrow, ncol } = this.data
-    const angleOffset = ANGLE_STEP * (ncol / 2 - 0.5) + 90
-
-    const phi = col * ANGLE_STEP - angleOffset
-    const phiRad = toRadians(phi)
-    this.el.setAttribute('rotation', {
-      y: -phi - 90
-    })
-    this.el.setAttribute('position', {
-      x: CYLINDER_RADIUS * Math.cos(phiRad),
-      y: (nrow - 1) / 2 - row,
-      z: CYLINDER_RADIUS * Math.sin(phiRad) + CAMERA_OFFSET
-    })
-
-    this.el.setAttribute('geometry', {
-      primitive: 'plane',
-      height: 0.8,
-      width: 0.8
-    })
-    this.el.setAttribute('material', {
-      color: '#FFFFFF'
-    })
+    setTileTransform(this)
+    setTileGeometry(this)
 
     this.el.addEventListener('click', function () {
       console.log('click')
@@ -39,6 +17,33 @@ export const tile = {
   }
 }
 
-function toRadians (degrees) {
-  return degrees / 180 * Math.PI
+const CYLINDER_RADIUS = 5
+const ANGLE_STEP = 12
+const CAMERA_OFFSET = 2.5
+
+function setTileTransform ({ data, el }) {
+  const { row, col, nrow, ncol } = data
+  const angleOffset = ANGLE_STEP * (ncol / 2 - 0.5) + 90
+
+  const phi = col * ANGLE_STEP - angleOffset
+  const phiRad = toRadians(phi)
+  el.setAttribute('rotation', {
+    y: -phi - 90
+  })
+  el.setAttribute('position', {
+    x: CYLINDER_RADIUS * Math.cos(phiRad),
+    y: (nrow - 1) / 2 - row,
+    z: CYLINDER_RADIUS * Math.sin(phiRad) + CAMERA_OFFSET
+  })
+}
+
+function setTileGeometry ({ el }) {
+  el.setAttribute('geometry', {
+    primitive: 'plane',
+    height: 0.8,
+    width: 0.8
+  })
+  el.setAttribute('material', {
+    color: '#FFFFFF'
+  })
 }
